9d5df92205487f180ca4c115f2835199eed2472b
yangzhihuilzy <yangzhihuilzy@sina.com>
2017-10-10 14:57:54 +0800

    base page is ok, data is ok

--- ok88/views.py
+++ ok88/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from models import article
+import json
 # Create your views here.
 
 def index(request):
@@ -15,4 +16,22 @@ def about(request):
 
 def detail(request, article_id):
     the_article = article.objects.get(article_id = article_id)
-    return render(request, 'ok88/detail.html', {'article':the_article})
+    print the_article.article_content
+
+    spans = json.loads(the_article.article_content)
+    out_spans = list()
+    for span in spans:
+        outspan = {}
+        if '{#br#}' in span['content']:
+            pass
+        else:
+            span['content'] = span['content'] + '{#br#}'
+        outspan['content1'] = span['content'].split('{#br#}')[0]
+        outspan['content2'] = span['content'].split('{#br#}')[1]
+        outspan['img'] = span['img']
+        #
+        # outspan['content'] = span['content'].replace('{#br#}', '</br><img src="{#br#}" alt="Picture" /></br>')
+        # outspan['content'] = outspan['content'].replace('{#br#}', span['img'])
+        out_spans.append(outspan)
+        print out_spans
+    return render(request, 'ok88/detail.html', {'article':the_article, 'out_spans':out_spans})
